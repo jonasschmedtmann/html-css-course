37261945d3699754a3e7b6389fda86e1f64fee6e
dathasai <dathasaichandu@gmail.com>
2022-07-15 17:08:44 +0530

    IntersectionObserver Sticky Navigation

--- starter/09-Omnifood-Optimizations/js/script.js
+++ starter/09-Omnifood-Optimizations/js/script.js
@@ -37,6 +37,29 @@ allLinks.forEach(function (link) {
   });
 });
 
+//sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
